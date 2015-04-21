412a2221c1d082539ca1ce3edc14ea50666ac177
Philip <pipcet@gmail.com>
2015-04-21 12:52:25 +0000

    turn a directory into a YAML document; then keep polling the directory and turning only those files that changed into a YAML document every second.
    works in /proc/sys. Probably doesn't work in /sys yet due to symlinks.

---
+++ yamlify.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+use YAML::XS;
+use Data::Dumper;
+use File::Slurp qw(slurp);
+
+# returns, recursively, entries in %$h that aren't in %$h2, or $h if
+# $h isn't a hash ref.
+sub hdiff {
+    my($h, $h2) = @_;
+    my $ret;
+
+    unless(ref $h eq "HASH") {
+	return ($h eq $h2) ? undef : $h;
+    }
+
+    for my $key (keys %$h) {
+	my $hd = hdiff($h->{$key}, $h2->{$key});
+
+	$ret->{$key} = $hd if defined $hd;
+    }
+
+    return $ret;
+}
+
+sub scan_dir {
+    my($dir) = @_;
+    my $h = { };
+
+    my $dh;
+    opendir $dh, $dir;
+    while(readdir $dh) {
+	my $path = $_;
+	next if $path =~ /^\.\.?$/;
+	$h->{$path} = scan("$dir/$path");
+    }
+    close $dh;
+
+    return $h;
+}
+
+sub scan_file {
+    my($file) = @_;
+
+    my $content;
+    eval {
+	$content = slurp($file);
+	chomp $content;
+    };
+
+    return $content;
+}
+
+sub scan {
+    my($path) = @_;
+
+    return scan_dir($path) if -d $path;
+    return scan_file($path) if -f $path;
+
+    die;
+}
+
+my $lastdata;
+while(1) {
+    my $data = scan(".");
+
+    warn YAML::XS::Dump(hdiff($data, $lastdata));
+    $lastdata = $data;
+    sleep(1);
+}
